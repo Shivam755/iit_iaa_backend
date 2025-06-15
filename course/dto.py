6366b6344ba51eb6d6e893043ebacb250d8665d1
shivam755 <www.shivam567@gmail.com>
2025-06-15 23:19:48 +0530

    modified the serializer to give extra fields

--- course/dto.py
+++ course/dto.py
@@ -2,15 +2,14 @@ from rest_framework import serializers
 from course.models import CourseInstance, Course
 from django.db import models
 
-class CourseViewInstanceDTO(serializers.Serializer):
+class CourseViewInstanceDTO(serializers.ModelSerializer):
     course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name="instances")
     course_title = serializers.CharField(source='course.title')
     course_code = serializers.CharField(source='course.course_code')
     course_id = serializers.CharField(source='course.id')
-    year_sem = serializers.SerializerMethodField()
     class Meta:
         model=CourseInstance
-        fields = ['id', 'year_sem', 'course_title', 'course_code', 'course_id']
+        fields = ['id', 'year', 'semester', 'course_title', 'course_code', 'course_id']
     
     def get_year_sem(self, obj):
         return f"{obj.year}-{obj.semester}"
